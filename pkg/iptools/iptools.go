07f1e1ceb77c3f48792eafd1f11f4a14a17d55d1
Daniel Hannon <hello111f@gmail.com>
2023-04-03 13:04:11 +0100

    Added STUN interface, rejigged iptools

--- pkg/iptools/iptools.go
+++ pkg/iptools/iptools.go
@@ -1,9 +1,13 @@
 package iptools
 
 import (
+	"encoding/binary"
+	"errors"
 	"fmt"
 	"regexp"
 	"strconv"
+
+	"github.com/danielh2942/goturn/pkg/stun"
 )
 
 const (
@@ -12,9 +16,9 @@ const (
 )
 
 type IpAddr interface {
+	stun.StunAttribute           // It implements all the methods of the StunAttribute interface
 	IpType() uint8               // Get the IP Address type
 	XorAddress([4]uint32) IpAddr // Create the XorMappedAddress
-	WriteAsStunAttr() []byte     // Output as a StunAttr
 }
 
 /*
@@ -33,12 +37,11 @@ func (Ipv4) IpType() uint8 {
 
 // This performs the XOR operation defined in rfc5389
 func (a Ipv4) XorAddress(val [4]uint32) IpAddr {
-	if a.xor {
-		return a
-	}
+	// Flip the Xor value
+	XorVal := a.xor != true
 	XordAddr := a.addr ^ val[0]
 	XordPort := a.port ^ uint16(val[0]>>16)
-	return Ipv4{xor: true, addr: XordAddr, port: XordPort}
+	return Ipv4{xor: XorVal, addr: XordAddr, port: XordPort}
 }
 
 // This outputs it as a stun attribute for the return packet
@@ -56,9 +59,38 @@ func (a Ipv4) WriteAsStunAttr() []byte {
 	return dataFrame
 }
 
+// Read in a stun attribute
+func (Ipv4) ParseStunAttr(data []byte) (stun.StunAttribute, uint16, error) {
+	// IPV4 Address type attributes are always 12 bytes, ipv6 are bigger.
+	if len(data) < 12 {
+		return Ipv4{}, 0, errors.New("invalid length")
+	}
+
+	attrVal := binary.BigEndian.Uint16(data)
+	if (attrVal != 0x0001) && (attrVal != 0x0020) {
+		return Ipv4{}, 0, errors.New("invalid attribute type")
+	}
+
+	xord := (attrVal == 0x0020)
+	msgLen := binary.BigEndian.Uint16(data[2:4])
+
+	if msgLen != 8 {
+		return Ipv4{}, 0, errors.New("invalid length")
+	}
+
+	if data[5] != 0x01 {
+		return Ipv4{}, 0, errors.New("Invalid data")
+	}
+
+	portNum := binary.BigEndian.Uint16(data[6:])
+	maddr := binary.BigEndian.Uint32(data[8:])
+
+	return Ipv4{xor: xord, port: portNum, addr: maddr}, 12, nil
+}
+
 func (a Ipv4) String() string {
 	var output string = ""
-	if (a.xor) {
+	if a.xor {
 		output = "Xor Mapped Address: "
 	}
 	return output + fmt.Sprintf("%d.%d.%d.%d:%d",
@@ -66,6 +98,16 @@ func (a Ipv4) String() string {
 	)
 }
 
+// AttributeType returns the STUN attribute type
+func (a Ipv4) AttributeType() stun.StunAttributeType {
+	if a.xor {
+		return stun.XOR_MAPPED_ADDRESS
+	} else {
+		return stun.MAPPED_ADDRESS
+	}
+}
+
+// ParseIpAddrString parses the strings as IP Addresses, it does not error check.
 func ParseIpAddrString(addr string) IpAddr {
 	// So far this only has IPV4 Support
 	// XOR-MAPPED address, first we get the IP Address
---
+++ pkg/stun/stun.go
@@ -0,0 +1,68 @@
+package stun
+
+type StunAttributeType uint16
+
+const (
+	UNKNOWN            StunAttributeType = 0xFFFF
+	MAPPED_ADDRESS     StunAttributeType = 0x0001
+	USERNAME           StunAttributeType = 0x0006
+	MESSAGE_INTEGRITY  StunAttributeType = 0x0008
+	ERROR_CODE		   StunAttributeType = 0x0009
+	UNKNOWN_ATTRIBUTES StunAttributeType = 0x000A
+	REALM              StunAttributeType = 0x0014
+	NONCE              StunAttributeType = 0x0015
+	XOR_MAPPED_ADDRESS StunAttributeType = 0x0020
+)
+
+func (s StunAttributeType) String() string {
+	switch s {
+	case MAPPED_ADDRESS:
+		return "MAPPED-ADDRESS"
+	case USERNAME:
+		return "USERNAME"
+	case MESSAGE_INTEGRITY:
+		return "MESSAGE_INTEGRITY"
+	case ERROR_CODE:
+		return "ERROR-CODE"
+	case UNKNOWN_ATTRIBUTES:
+		return "UNKNOWN-ATTRIBUTES"
+	case REALM:
+		return "REALM"
+	case NONCE:
+		return "NONCE"
+	case XOR_MAPPED_ADDRESS:
+		return "XOR-MAPPED-ADDRESS"
+	}
+	return "UNKNOWN"
+}
+
+func ParseStunAttrType(val uint16) StunAttributeType {
+	switch val {
+	case 0x0001:
+		return MAPPED_ADDRESS
+	case 0x0006:
+		return USERNAME
+	case 0x0008:
+		return MESSAGE_INTEGRITY
+	case 0x0009:
+		return ERROR_CODE
+	case 0x000A:
+		return UNKNOWN_ATTRIBUTES
+	case 0x0014:
+		return REALM
+	case 0x0015:
+		return NONCE
+	case 0x0020:
+		return XOR_MAPPED_ADDRESS
+	default:
+		return UNKNOWN
+	}
+}
+
+// StunAttribute is a generalized form for stun attributes and stuff
+// It is done to make it easier to parse and understand STUN attributes
+type StunAttribute interface {
+	AttributeType() StunAttributeType
+	ParseStunAttr([]byte) (StunAttribute, uint16, error) // Read in STUN Attribute
+	WriteAsStunAttr() []byte                             // Output as attribute for stun packet
+}
